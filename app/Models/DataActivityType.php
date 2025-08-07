581a26c7162fdb18665576dc420fcea1680a3ccf
Sanuuu24 <satriaibnusaparudin@gmail.com>
2025-08-07 16:12:00 +0700

    fix: activity type models

--- app/Models/DataActivityType.php
+++ app/Models/DataActivityType.php
@@ -9,4 +9,9 @@ class DataActivityType extends Model
     protected $fillable = [
         'type_name',
     ];
+
+    public function activities()
+    {
+        return $this->hasMany(DataActivity::class, 'activity_type_id');
+    }
 }
