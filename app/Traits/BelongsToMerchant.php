fcb2471e1af30e15ab3b50af1486c5ce63e899ff
Sanuuu24 <satriaibnusaparudin@gmail.com>
2025-08-13 14:02:42 +0700

    feat: filtering option admin merchant id, fix: error package intervention/image

--- app/Traits/BelongsToMerchant.php
+++ app/Traits/BelongsToMerchant.php
@@ -8,19 +8,16 @@ use Illuminate\Support\Facades\Auth;
 
 trait BelongsToMerchant
 {
-    protected static function bootBelongsToMerchant()
+    protected static function bootBelongsToMerchant(): void
     {
-        // Scope untuk memfilter data saat dibaca (SELECT)
         static::addGlobalScope('merchant', function (Builder $builder) {
             if (Auth::check() && Auth::user() instanceof Admin) {
                 $builder->where(self::getTable() . '.merchant_id', Auth::user()->merchant_id);
             }
         });
 
-        // Event untuk mengisi merchant_id secara otomatis saat data dibuat (INSERT)
         static::creating(function ($model) {
             if (Auth::check() && Auth::user() instanceof Admin) {
-                // Hanya isi jika merchant_id belum diatur sebelumnya
                 if (!$model->merchant_id) {
                     $model->merchant_id = Auth::user()->merchant_id;
                 }
